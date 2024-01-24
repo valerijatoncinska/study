f83e3d774d6b62e55758637953eeba62c43e67ee
Sunny Capt <sunny.capt@tuta.io>
2024-01-24 18:57:52 +0100

    Update 24.01.2024.java

--- Homework /basic java /24.01.2024.java
+++ Homework /basic java /24.01.2024.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-class CurrencyConverterButBetter {
+public class CurrencyConverterButBetter {
     public static void main(String[] args) {
         String originalCurrency;
 
